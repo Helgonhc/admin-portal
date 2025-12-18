1d7696f1287dc2767791f8d52e7b71fe4be2adf5
Helgonhc <helgonhc19@yahoo.com.br>
2025-12-18 03:05:58 -0300

    Fix: atualizar Next.js para 14.1.0 e adicionar .vercelignore

--- next.config.js
+++ next.config.js
@@ -9,15 +9,6 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  experimental: {
-    outputFileTracingExcludes: {
-      '*': [
-        'node_modules/@swc/core-linux-x64-gnu',
-        'node_modules/@swc/core-linux-x64-musl',
-        'node_modules/@esbuild/linux-x64',
-      ],
-    },
-  },
 };
 
 module.exports = nextConfig;
